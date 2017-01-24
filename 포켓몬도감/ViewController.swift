565aa7bab7bb7f66f9e25a03cda508640b9fc49e
YongHui Cho <ycho46@gmail.com>
2017-01-24 17:58:33 -0500

    first commit

---
+++ 포켓몬도감/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  포켓몬도감
+//
+//  Created by YongHui Cho on 1/24/17.
+//  Copyright © 2017 Adnap. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
